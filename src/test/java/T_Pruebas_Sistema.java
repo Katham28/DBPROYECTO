17c73300c449af6e523f9f07b89cb9a4fedbf2b8
ul1sesHD <ulises.hd06@gmail.com>
2025-11-13 10:03:13 -0600

    feat(test): Añadida Suite de pruebas T_Pruebas_Sistema para agrupar todas las pruebas funcionales del sistema

---
+++ src/test/java/T_Pruebas_Sistema.java
@@ -0,0 +1,22 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	T_Pantalla_Menu_principal.class,
+	T_Pantalla_Crear_Cuenta.class,
+	T_Pantalla_Iniciar_Sesion.class,
+	T_Pantalla_Recuperar_Password.class,
+	T_Pantalla_Menu_Usuario.class,
+	T_Pantalla_Gatcha.class,
+	T_Pantalla_Ajustes_Usuario.class,
+	T_Pantalla_Ingresar_Admin.class,
+	T_Pantalla_Mod_Eli_Admin.class,
+})
+public class T_Pruebas_Sistema {
+
+	public T_Pruebas_Sistema() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
